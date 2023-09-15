bce513420a364fcb5133dde8234687e28509b048
Zizi_Kim <rlarjsdn3@naver.com>
2023-09-15 17:03:01 +0900

    ValorantShopFileManagerTestProject 프로젝트 추가

---
+++ ValorantShopFileManagerTestProject/ValorantShopFileManagerTestProject/ValorantShopFileManagerTestProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  ValorantShopFileManagerTestProjectApp.swift
+//  ValorantShopFileManagerTestProject
+//
+//  Created by 김건우 on 2023/09/15.
+//
+
+import SwiftUI
+
+@main
+struct ValorantShopFileManagerTestProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
